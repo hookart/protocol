8bbdf9d2dc16702bcc19bb0ea8714b88c13b38b5
Regynald Augustin <github@regynald.com>
2022-04-22 13:09:58 -0400

    Forgot a file

--- src/interfaces/IHookCoveredCallFactory.sol
+++ src/interfaces/IHookCoveredCallFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 interface IHookCoveredCallFactory {
     // TODO(HOOK-802) Migrate natspec docs to interfaces instead of implementations, inherit on implementations
