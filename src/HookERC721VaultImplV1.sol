142e189a1cb6788045baae779a1e8c8168666d82
Jacob R. Nyquist <jake@nyqu.ist>
2022-05-04 12:38:12 -0400

    created interface for faults that includes ierc721'

--- src/HookERC721VaultImplV1.sol
+++ src/HookERC721VaultImplV1.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
