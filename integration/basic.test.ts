0fd38bf45605b3b507d56304539d11b09494e5ee
Jacob R. Nyquist <jake@nyqu.ist>
2022-05-23 11:34:50 -0400

    proto commit

--- integration/basic.test.ts
+++ integration/basic.test.ts
@@ -2,41 +2,15 @@ import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { Contract, Signer } from 'ethers';
 
-describe('Mover', function () {
-  let mover: Contract, nft: Contract;
-  let alice: Signer, bob: Signer;
-
-  before(async () => {
-    const MockNFT = await ethers.getContractFactory('MockNFT');
-    nft = await MockNFT.deploy();
-    await nft.deployed();
-
-    const Mover = await ethers.getContractFactory('Mover');
-    mover = await Mover.deploy();
-    await mover.deployed();
-
-    [, alice, bob] = await ethers.getSigners();
-
-    let balance = await nft.balanceOf(await alice.getAddress()); 
-    expect(balance.toNumber()).eq(0);
-
-    await nft.connect(alice).mint(2);
-
-    balance = await nft.balanceOf(await alice.getAddress()); 
-    expect(balance.toNumber()).eq(2);
-    await nft.connect(alice).setApprovalForAll(mover.address, true);
-  });
-
-  it('should move many NFTs', async () => {
-    let address = await bob.getAddress();
-
-    let balance = await nft.balanceOf(address);
-    expect(balance.toNumber()).eq(0);
-
-    await mover.connect(alice).moveBatch(nft.address, [1, 2], address);
-
-    balance = await nft.balanceOf(address);
-    expect(balance.toNumber()).eq(2);
-  });
+describe("Deploy", function() {
+    it("the protocol should deploy", async function() {
+      const [admin] = await ethers.getSigners();
+      console.log('got signers')
+    const weath = await ethers.getContractFactory("WETH");
+    const weth = await weath.deploy();
+    console.log(weth.address);
+      const HookProtocol = await ethers.getContractFactory("HookProtocol");
   
-})
+      const protocol = await HookProtocol.deploy(admin.address, weth.address);
+    });
+  });
