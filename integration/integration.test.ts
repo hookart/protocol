6f03f971768cffde2530b34da8b173d53c6723be
Eliecer Chicott <eliecerchicott@Eliecers-MacBook-Pro-2.local>
2022-05-23 22:36:05 -0300

    WIP

--- integration/integration.test.ts
+++ integration/integration.test.ts
@@ -3,6 +3,21 @@ import { ethers } from "hardhat";
 import type { Contract, PayableOverrides } from "ethers";
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
+// TODO: move to helpers later
+const SECS_IN_A_DAY = 60 * 60 * 24;
+
+export function getEpochTimestampDaysFromNow(days: number) {
+  const nowEpoch = Date.now() / 1000;
+  const epochDaysFromNow = nowEpoch + SECS_IN_A_DAY * days;
+
+  return Math.floor(epochDaysFromNow);
+}
+
+// needs extra gas limit since proxied contracts are hard to calculate
+const overrides: PayableOverrides = {
+  gasLimit: "1000000",
+};
+
 describe("Integrations", function () {
   let vaultFactory: Contract,
     protocol: Contract,
@@ -148,17 +163,10 @@ describe("Integrations", function () {
 
     expect(isApproved).to.equal(true, "Call instrument approval didn't work.");
 
-    const nowEpoch = Date.now() / 1000;
-    const SECS_IN_A_DAY = 60 * 60 * 24;
-    // needs extra gas limit since proxied contracts are hard to calculate
-    const overrides: PayableOverrides = {
-      gasLimit: "1000000",
-    };
-
     const expiryArr = [
-      Math.floor(nowEpoch + SECS_IN_A_DAY * 2), // around 2 days from now
-      Math.floor(nowEpoch + SECS_IN_A_DAY * 15), // around 15 days from now
-      Math.floor(nowEpoch + SECS_IN_A_DAY * 30), // around 30 days from now
+      Math.floor(getEpochTimestampDaysFromNow(2)), // around 2 days from now
+      Math.floor(getEpochTimestampDaysFromNow(15)), // around 15 days from now
+      Math.floor(getEpochTimestampDaysFromNow(30)), // around 30 days from now
     ];
 
     for (let i = 0; i < expiryArr.length; i++) {
@@ -185,6 +193,68 @@ describe("Integrations", function () {
       );
     }
   });
+
+  it("mint option using vault", async function () {
+    // Mint NFT
+    const tokenId = 4;
+    await token.mint(writer.address, tokenId);
+
+    const ownerOf4 = await token.ownerOf(tokenId);
+
+    expect(ownerOf4).to.equal(
+      writer.address,
+      "Owner of minted token didn't match."
+    );
+
+    // Put in vault
+    const tx = await vaultFactory.makeSoloVault(token.address, tokenId);
+    const receipt = await tx?.wait();
+    const event = (receipt as any)?.events?.find(
+      ({ event }: any) => event === "ERC721VaultCreated"
+    );
+    const vault = event?.args?.vaultAddress;
+    await token
+      .connect(writer)
+      ["safeTransferFrom(address,address,uint256)"](
+        writer.address,
+        vault,
+        tokenId
+      );
+    const vaultInstance = await ethers.getContractAt(
+      "HookERC721VaultImplV1",
+      vault
+    );
+
+    expect(await vaultInstance.getBeneficialOwner(tokenId)).eq(writer.address);
+   
+
+    // Sign Entitlement
+    // const signature = await signEntitlement(
+    //   address,
+    //   callInstrument,
+    //   tokenContractAddress,
+    //   tokenId,
+    //   expiry,
+    //   provider
+    // );
+
+    // Mint with vault
+    const mintTx = await coveredCallImplInstance.connect(writer).mintWithVault(
+      vault,
+      tokenId,
+      1000, // strike price in wei
+      Math.floor(getEpochTimestampDaysFromNow(2)), // expiry
+      ,
+      overrides
+    );
+    const mintReceipt = await mintTx?.wait();
+  });
+
+  it("mint option using an entitled vault", async function () {});
+
+  it("mint option using an unentitled vault and a signature", async function () {});
+
+  it("reclaim asset", async function () {});
 
   //   it("protocol was deployed", async function () {
   //     const confirmations = await protocol?.deployTransaction?.confirmations;
