af3ea436c3b546fba84bd7f7caf86b7e0cbd27e6
Regynald Augustin <github@regynald.com>
2022-05-24 12:41:24 -0400

    [WIP] Call instrument tests (#29)
    * vault coverage tests
    
    * 100% vault factory coverage
    
    * empty case
    
    * transfers
    
    * Adding js helpers for singing Entitlement
    
    * chainId for hardhat network
    
    * up to 68% test coverage
    
    * 85% coverage
    
    * checkpoint
    
    * 100% lines coverage in vaults
    
    * Update gitignore
    
    WIP tests
    
    Working mintwitherc721 tests
    
    Use before each
    
    Most mintWithVault and mintWithEntitledVault tests
    
    mintWithEntitledVault coverage
    
    bid coverage
    
    settle coverage
    
    First reclaim test
    
    * move tests into a single file
    
    * add write with signature test (failing)
    
    Co-authored-by: Jacob R. Nyquist <jake@nyqu.ist>
    Co-authored-by: Eliecer Chicott <eliecerchicott@Eliecers-MacBook-Pro-2.local>

--- integration/basic.test.ts
+++ integration/basic.test.ts
@@ -1,16 +1,14 @@
-import { expect } from 'chai';
-import { ethers } from 'hardhat';
-import { Contract, Signer } from 'ethers';
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Contract, Signer } from "ethers";
 
-describe("Deploy", function() {
-    it("the protocol should deploy", async function() {
-      const [admin] = await ethers.getSigners();
-      console.log('got signers')
-    const weath = await ethers.getContractFactory("WETH");
-    const weth = await weath.deploy();
-    console.log(weth.address);
-      const HookProtocol = await ethers.getContractFactory("HookProtocol");
-  
-      const protocol = await HookProtocol.deploy(admin.address, weth.address);
-    });
+describe("Deploy", function () {
+  it("the protocol should deploy", async function () {
+    const [admin] = await ethers.getSigners();
+    const wethFactory = await ethers.getContractFactory("WETH");
+    const weth = await wethFactory.deploy();
+    const HookProtocol = await ethers.getContractFactory("HookProtocol");
+
+    const protocol = await HookProtocol.deploy(admin.address, weth.address);
   });
+});
