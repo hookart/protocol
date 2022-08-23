142d532fa8e90fc0d5d840fab4ac65629f24d9f8
Jacob R. Nyquist <jake@nyqu.ist>
2022-08-23 14:49:58 -0400

    cleanup deployer scripts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,13 +1,12 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
-// import { LedgerSigner } from "@anders-t/ethers-ledger";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   console.log("what is happening");
-  // const signer = new LedgerSigner(ethers.provider);
+
   let {
     deployer,
     vaultUpgrader,
@@ -24,133 +23,131 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   console.log("Deploying from", deployer);
 
-  // proxy only in non-live network (localhost and hardhat network) enabling HCR (Hot Contract Replacement)
-  // in live network, proxy is disabled and constructor is invoked
+  console.log("Deploying with these args:", [
+    allowlister,
+    pauserRole,
+    vaultUpgrader,
+    callsUpgrader,
+    marketConf,
+    collectionConf,
+    weth,
+  ]);
 
-  // console.log("Deploying with these args:", [
-  //   allowlister,
-  //   pauserRole,
-  //   vaultUpgrader,
-  //   callsUpgrader,
-  //   marketConf,
-  //   collectionConf,
-  //   weth,
-  // ]);
+  const protocol = await deploy("HookProtocol", {
+    from: deployer,
+    args: [
+      allowlister,
+      pauserRole,
+      vaultUpgrader,
+      callsUpgrader,
+      marketConf,
+      collectionConf,
+      weth,
+    ],
+    log: true,
+    autoMine: true,
+  });
 
-  // const protocol = await deploy("HookProtocol", {
-  //   from: deployer,
-  //   args: [
-  //     allowlister,
-  //     pauserRole,
-  //     vaultUpgrader,
-  //     callsUpgrader,
-  //     marketConf,
-  //     collectionConf,
-  //     weth,
-  //   ],
-  //   log: true,
-  //   autoMine: true,
-  // });
+  const protocolImpl = await ethers.getContractAt(
+    "HookProtocol",
+    protocol.address
+  );
 
-  // const protocolImpl = await ethers.getContractAt(
-  //   "HookProtocol",
-  //   protocol.address
-  // );
+  const soloVault = await deploy("HookERC721VaultImplV1", {
+    from: deployer,
+    args: [],
+    log: true,
+    autoMine: true,
+  });
 
-  // const soloVault = await deploy("HookERC721VaultImplV1", {
-  //   from: deployer,
-  //   args: [],
-  //   log: true,
-  //   autoMine: true,
-  // });
+  const multiVault = await deploy("HookERC721MultiVaultImplV1", {
+    from: deployer,
+    args: [],
+    log: true,
+    autoMine: true,
+  });
 
-  // const multiVault = await deploy("HookERC721MultiVaultImplV1", {
-  //   from: deployer,
-  //   args: [],
-  //   log: true,
-  //   autoMine: true,
-  // });
+  const multiVaultBeacon = await deploy("HookUpgradeableBeacon", {
+    from: deployer,
+    args: [
+      multiVault.address,
+      protocol.address,
+      ethers.utils.id("VAULT_UPGRADER"),
+    ],
+    log: true,
+    autoMine: true,
+  });
 
-  // const multiVaultBeacon = await deploy("HookUpgradeableBeacon", {
-  //   from: deployer,
-  //   args: [
-  //     multiVault.address,
-  //     protocol.address,
-  //     ethers.utils.id("VAULT_UPGRADER"),
-  //   ],
-  //   log: true,
-  //   autoMine: true,
-  // });
+  const soloVaultBeacon = await deploy("HookUpgradeableBeacon", {
+    from: deployer,
+    args: [
+      soloVault.address,
+      protocol.address,
+      ethers.utils.id("VAULT_UPGRADER"),
+    ],
+    log: true,
+    autoMine: true,
+  });
 
-  // const soloVaultBeacon = await deploy("HookUpgradeableBeacon", {
-  //   from: deployer,
-  //   args: [
-  //     soloVault.address,
-  //     protocol.address,
-  //     ethers.utils.id("VAULT_UPGRADER"),
-  //   ],
-  //   log: true,
-  //   autoMine: true,
-  // });
+  const vaultFactory = await deploy("HookERC721VaultFactory", {
+    from: deployer,
+    args: [protocol.address, soloVaultBeacon.address, multiVaultBeacon.address],
+    log: true,
+    autoMine: true,
+  });
 
-  // const vaultFactory = await deploy("HookERC721VaultFactory", {
-  //   from: deployer,
-  //   args: [protocol.address, soloVaultBeacon.address, multiVaultBeacon.address],
-  //   log: true,
-  //   autoMine: true,
-  // });
+  if (vaultUpgrader === deployer) {
+    const vfSet = await protocolImpl.setVaultFactory(vaultFactory.address);
+    console.log("Set vault factory onto protocol with hash: ", vfSet.hash);
+  }
 
-  // const vfSet = await protocolImpl.setVaultFactory(vaultFactory.address);
+  const font1 = await deploy("Font1", {
+    from: deployer,
+    args: [],
+    log: true,
+    // maxPriorityFeePerGas: "2000000000",
+    // maxFeePerGas: "50000000000",
+    autoMine: true,
+  });
+  const font2 = await deploy("Font2", {
+    from: deployer,
+    args: [],
+    log: true,
+    // maxPriorityFeePerGas: "2000000000",
+    // maxFeePerGas: "50000000000",
+    autoMine: true,
+  });
+  const font3 = await deploy("Font3", {
+    from: deployer,
+    args: [],
+    log: true,
+    // maxPriorityFeePerGas: "2000000000",
+    // maxFeePerGas: "50000000000",
+    autoMine: true,
+  });
 
-  // console.log("Set vault factory onto protocol with hash: ", vfSet.hash);
-
-  // const font1 = await deploy("Font1", {
-  //   from: deployer,
-  //   args: [],
-  //   log: true,
-  //   // maxPriorityFeePerGas: "2000000000",
-  //   // maxFeePerGas: "50000000000",
-  //   autoMine: true,
-  // });
-  // const font2 = await deploy("Font2", {
-  //   from: deployer,
-  //   args: [],
-  //   log: true,
-  //   // maxPriorityFeePerGas: "2000000000",
-  //   // maxFeePerGas: "50000000000",
-  //   autoMine: true,
-  // });
-  // const font3 = await deploy("Font3", {
-  //   from: deployer,
-  //   args: [],
-  //   log: true,
-  //   // maxPriorityFeePerGas: "2000000000",
-  //   // maxFeePerGas: "50000000000",
-  //   autoMine: true,
-  // });
-
-  // const tokenURI = await deploy("TokenURI", {
-  //   from: deployer,
-  //   args: [],
-  //   libraries: {
-  //     Font1: font1.address, // "0x1Ac06Ef3cda4dC2CB30A866090041D3266c33d45",
-  //     Font2: font2.address, //"0xfa10218700bFd179DE800a461C98357b39525f38",
-  //     Font3: font3.address, //"0x4C6eDA9CBb9B31152f3f002CAe5E3eF805Ad19f9",
-  //   },
-  //   log: true,
-  //   // maxPriorityFeePerGas: "2000000000",
-  //   // maxFeePerGas: "50000000000",
-  //   autoMine: true,
-  // });
-  // const callV1 = await deploy("HookCoveredCallImplV1", {
-  //   from: deployer,
-  //   libraries: {
-  //     TokenURI: tokenURI.address,
-  //   },
-  //   args: [],
-  //   log: true,
-  //   autoMine: true,
-  // });
+  const tokenURI = await deploy("TokenURI", {
+    from: deployer,
+    args: [],
+    libraries: {
+      Font1: font1.address, // "0x1Ac06Ef3cda4dC2CB30A866090041D3266c33d45",
+      Font2: font2.address, //"0xfa10218700bFd179DE800a461C98357b39525f38",
+      Font3: font3.address, //"0x4C6eDA9CBb9B31152f3f002CAe5E3eF805Ad19f9",
+    },
+    log: true,
+    // maxPriorityFeePerGas: "2000000000",
+    // maxFeePerGas: "50000000000",
+    autoMine: true,
+  });
+  const callV1 = await deploy("HookCoveredCallImplV1", {
+    from: deployer,
+    libraries: {
+      TokenURI: tokenURI.address,
+    },
+    args: [],
+    log: true,
+    autoMine: true,
+  });
 
   const callBeacon = await deploy("HookUpgradeableBeacon", {
     from: deployer,
@@ -174,11 +171,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     autoMine: true,
   });
 
-  // const cfSet = await protocolImpl.setCoveredCallFactory(callFactory.address);
+  if (deployer === callsUpgrader) {
+    const cfSet = await protocolImpl.setCoveredCallFactory(callFactory.address);
+    console.log("Set call factory onto protocol with hash: ", cfSet.hash);
+  }
 
-  // console.log("Set call factory onto protocol with hash: ", cfSet.hash);
+  if (deployer == pauserRole) {
+    // Will need to pause outside of this context
+    // for the process to work with mainnet deploys
+    await protocolImpl.connect(deployer).pause();
+  }
 
-  // await protocolImpl.connect(deployer).unpause();
   return true;
 };
 export default func;
