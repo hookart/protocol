bff6ec368be82ec9b616af1c588dea3b8018fa7c
Jacob R. Nyquist <jake@nyqu.ist>
2022-08-23 02:08:37 -0400

    deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,14 +7,15 @@ import "@nomiclabs/hardhat-etherscan";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "hardhat-deploy";
-import "hardhat-deploy-ethers";
 import "hardhat-docgen";
 import { HardhatUserConfig, subtask } from "hardhat/config";
 import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from "hardhat/builtin-tasks/task-names";
+// import { LedgerSigner } from "@anders-t/ethers-ledger";
 
 // default values here to avoid failures when running hardhat
 const ROPSTEN_RPC = process.env.ROPSTEN_RPC || "1".repeat(32);
 const GOERLI_RPC = process.env.GOERLI_RPC || "1".repeat(32);
+const MAINNET_RPC = process.env.MAINNET_RPC || "1".repeat(32);
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "1".repeat(64);
 const SOLC_DEFAULT = "0.8.10";
 
@@ -41,7 +42,7 @@ subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
   }
 );
 
-const config: HardhatUserConfig = {
+const config: any = {
   docgen: {
     path: "./docs/generated",
     clear: true,
@@ -65,6 +66,9 @@ const config: HardhatUserConfig = {
       url: GOERLI_RPC,
       accounts: [PRIVATE_KEY],
     },
+    mainnet: {
+      url: MAINNET_RPC,
+    },
   },
   solidity: {
     version: foundry.default?.solc || SOLC_DEFAULT,
@@ -83,11 +87,17 @@ const config: HardhatUserConfig = {
     coinmarketcap: process.env.CMC_KEY ?? "",
   },
   namedAccounts: {
-    deployer: "0xF9F6009a5f2511f0C859927e43fC12745dc4426a",
+    deployer: "ledger://0x1cAA0034b17786E18D94Ca176b1F8ec3F7972908",
     weth: process.env.WETH_ADDRESS || "",
     approvedMarket:
       process.env.APROVED_MARKET ||
       "0xdef1c0ded9bec7f1a1670819833240f027b25eff",
+    vaultUpgrader: process.env.VAULT_UPGRADER || "",
+    callsUpgrader: process.env.CALLS_UPGRADER || "",
+    pauserRole: process.env.PAUSER_ROLE || "",
+    marketConf: process.env.MARKET_CONF || "",
+    collectionConf: process.env.COLLECTION_CONF || "",
+    allowlister: process.env.ALLOWLISTER || "",
   },
   etherscan: {
     // API key for Etherscan. https://etherscan.io/
